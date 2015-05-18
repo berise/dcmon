dd837c7782349cd75a2d910ce210151a29911233
berise <berise@gmail.com>
2015-05-18 09:12:32 +0900

    weekend progresses

--- dcmon2.py
+++ dcmon2.py
@@ -8,9 +8,11 @@ import os
 import Queue
 import logging
 
+import selenium
 from selenium import webdriver
 import pprint
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 
 import module_locator
@@ -65,7 +67,7 @@ class ThreadWorker(threading.Thread):
 
 
 
-class Monitor():
+class Robot():
     """
     kGALLERY_NAME = "given as an arg"
     """
@@ -133,63 +135,152 @@ class Monitor():
         return sb_driver
 
 
-    def browse_category_and_collect(self, url):
-        start_page = 1
-        #
-        curr_url = url
-        next_page = start_page + 1
-        while True:
-            self.collect_apk_urls(self.web_driver, curr_url)
+    def get_page_no(self, url, list):
+        self.web_driver.get(url)
 
-            logging.info("[collect_urls] URL(page) : %s(%d), " % (curr_url, next_page))
-            logging.info("[collect_urls] collected url count : %d" % len(self.app_urls))
-            pprint.pprint(self.app_urls)
-
-            (ret, next_url) = self.get_next_page(self.web_driver, next_page)
-            if ret:
-                curr_url = next_url
-                next_page += 1
-            else:
-                break
-
-    def collect_apk_urls(self, web_driver, url):
-        web_driver.get(url)
+        # css path to thread
+        #'#dgn_gallery_left > div.gallery_list > div.list_table > table > thead'
 
         try:
-            ul_elements = web_driver.find_element_by_css_selector('#appSoftListBox')
+            dom_thread = self.web_driver.find_element_by_css_selector('#dgn_gallery_left > div.gallery_list > div.list_table > table > thead')
+            dom_trs = dom_thread.find_elements_by_tag_name('tr')
 
-            lis = ul_elements.find_elements_by_tag_name('li')
-            for li in lis:
-                a = li.find_element_by_tag_name('a')
-                href = a.get_attribute('href')
-                self.app_urls[href] = 1  # duplicated url will be removed
-        except:
-            logging.info("[collect_apk_urls] find_element_by_css_selector failed.")
+            for dom_tr in dom_trs:
+                try:
+                    # first t_notice is not a number
+                    e1 = dom_tr.find_element_by_class_name('t_notice')
+                except:
+                        logging.error("t_notice is not found")
+                else:
+                    #print (e1.text)
+                    if is_digit(e1.text):
+                        list.append(int(e1.text))
+                    else:
+                        logging.info("pass on non numeric element %s", e1.text)
+
+        except selenium.common.exceptions.NoSuchElementException:
+            return
+
+    def get_page_no(self, url, list):
+        self.web_driver.get(url)
+
+        # css path to thread
+        #'#dgn_gallery_left > div.gallery_list > div.list_table > table > thead'
+        try:
+            dom_thread = self.web_driver.find_element_by_css_selector('#dgn_gallery_left > div.gallery_list > div.list_table > table > thead')
+            dom_trs = dom_thread.find_elements_by_tag_name('tr')
+
+            for dom_tr in dom_trs:
+                try:
+                    # first t_notice is not a number
+                    e1 = dom_tr.find_element_by_class_name('t_notice')
+                except:
+                        logging.error("t_notice is not found")
+                else:
+                    #print (e1.text)
+                    if is_digit(e1.text):
+                        list.append(int(e1.text))
+                    else:
+                        logging.info("pass on non numeric element %s", e1.text)
+
+        except selenium.common.exceptions.NoSuchElementException:
+            return
+
+    def compare_no(self, l1, l2):
+        for i in range(len(l1)):
+            if l1[i] != l2[i]:
+                return i
+        return -1
+
+    def monitor_and_get(self, url):
+        list1 = []
+        list2 = []
+        self.get_page_no(url, list1)
+        list2 = list1
+
+        while(1):
+            self.get_page_no(url, list2)
+
+            new_article_list = list(set(list2) - set(list1))
+            #new_no = self.compare_no(list1, list2)
+
+
+            # to make a full url
+            #http://gall.dcinside.com/board/view/?id=game_classic&no=6349933&page=1
+            if new_article_list:
+                print (new_article_list)
+            for i in new_article_list:
+            #target_url = "{0}&no={1}&page=1".format(kGALLERIES[0], i)
+            # down in a new tab
+                #body = self.web_driver.find_element_by_tag_name("body")
+                #ActionChains(self.web_driver).send_keys(Keys.CONTROL, "t").perform()
+
+                target_url = "http://gall.dcinside.com/board/view/?id=game_classic&no={0}&page=1".format(i)
+                print (target_url)
+                self.download_if_attached(target_url)
+
+            # update list
+            list1 = list2
+
+            # sleep little time
+            time.sleep(random.randrange(2, 3))
 
 
     def download_if_attached(self, url):
+        """
+        attached file css path : #dgn_content_de > div.re_gall_box_3 > div > ul
+        XPATH : //*[@id="dgn_content_de"]/div[5]/div/ul
+
+        :param url:
+        :return:
+        """
         self.web_driver.get(url)
         #logging.info("Wait for 3 seconds till web page download")
 
-        WebDriverWait(self.web_driver, 3)
+        #WebDriverWait(self.web_driver, 3)
 
-        ul_elements = self.web_driver.find_element_by_css_selector('#dgn_content_de > div.re_gall_box_3 > div > ul')
+        file_css_selectors = [ '#dgn_content_de > div.re_gall_box_3 > div > ul' ]
+        #file_xpath_selectors = [ '//*[@id="dgn_content_de"]/div[5]/div/ul' ]
 
-        lis = ul_elements.find_elements_by_tag_name('li')
-        print (lis)
+        try:
+            for css in file_css_selectors:
+                ul_elements = self.web_driver.find_element_by_css_selector(css)
 
-        for li in lis:
-            a = li.find_element_by_tag_name('a')
-            print (a.text)
+        except selenium.common.exceptions.NoSuchElementException:
+            return
+        else:
+            lis = ul_elements.find_elements_by_tag_name('li')
+            #print (lis)
+
+            for li in lis:
+                a = li.find_element_by_tag_name('a')
+
             a.click()
-            time.sleep(random.randrange(1, 2))
-            #href = a.get_attribute('href')
-            #print (href)
-            #href.click()
+            #time.sleep(random.randrange(1, 2))
+            #except:
+            #    logging.error("%s : no such css element", url)
+
+            #try:
+        # for xpath in file_xpath_selectors:
+        #     ul_elements = self.web_driver.find_element_by_xpath(xpath)
+        #
+        # lis = ul_elements.find_elements_by_tag_name('li')
+        # #print (lis)
+        #
+        # for li in lis:
+        #     a = li.find_element_by_tag_name('a')
+        #
+        #     a.click()
+            #time.sleep(random.randrange(1, 2))
+        #except:
+        #    logging.error("%s : no such xpath element", url)
+
 
         # wait a file being downloaded for 3 seconds.
         # make it a longer if file is broken
-        WebDriverWait(self.web_driver, 3)
+        #WebDriverWait(self.web_driver, 3)
+
+        #self.web_driver.close()
 
 
     def get_next_page(self, web_driver, next_page):
@@ -217,139 +308,13 @@ class Monitor():
         return (False, None)
 
 
-
-    def run_collector_url(self):
-        web_driver = self.init_selenium_driver("chrome", self.chrome_options)
-
-        # overall process as follows.
-        # do the similar jobs as styner did. (gather urls and put them all in db and process each records)
-
-        # start_urls???�회?�면???�집??url(app_urls)??db???��? ??app_urls 초기??
-        for url in self.start_urls:
-            self.browse_category_and_collect(web_driver, url)
-
-            for k, v in self.app_urls.iteritems():
-                print "db_insert_url(%d) insert %s, %s into db" % (ret, k, v)
-
-            self.app_urls.clear()
-
-
-        web_driver.quit()
-
-
-    def run_collector_downloader(self):
-        """
-            download apk in the database
-        """
-        logging.info("init selenium driver %s", "chrome")
-        web_driver = self.init_selenium_driver("chrome", self.chrome_options)
-
-        # to watch download path
-        #threading.Thread(group=None, target=watch, args=[self.chrome_options['download_folder'], cb_file_watch])
-
-        # thread_watcher�?while?�에 ?�을 경우??문제??
-        # URL??리소?��? ?�는 경우 timeout??발생?�며(60*60) ??경우 thread_watcher???�상 종료?��? ?�음.
-        # ?�라??watcher??종료 ?�이 계속 모니?�링?�는 ?�몬 ?�식?�로 �?��
-        stop_event = threading.Event()
-        logging.info("[downloader] start a watch thread")
-        thread_watcher = ThreadWorker(watch, stop_event, collector.chrome_options['download_folder'], self.downloaded_files)
-        thread_watcher.start()      # daemonize will also works
-
-        ###
-        logging.info("[downloader] start a user action thread")
-        #ret = self.download_apk(web_driver, row['url'])
-        #ThreadWorker(self.download_apk, web_driver, row['url']).start()
-        #thread_simulator = ThreadWorker(self.simulate_user_action, web_driver, row['url'])
-        thread_simulator = ThreadWorker(self.simulate_user_action, web_driver, row['url'])
-        thread_simulator.start()
-        thread_simulator.join()
-
-        # ?�레?��? ?�성????DirectoryChange?�벤?��? 기다린다
-        wait_time = 60 * 30   # 60 sec * 30 = .5 hour
-        #wait_time = 6   # test
-        logging.info("[downloader] Wait stop_event from watch thread. %d seconds left", wait_time)
-        ret_wait = stop_event.wait(wait_time)
-        stop_event.clear()      # Subsequently, threads calling wait() will block until set() is called to set the internal flag to true again.
-
-#           if ret_wait == True:        # stop_event is set(notified)
-
-#               time.sleep(5)       # let browser have some time to finish download
-#               logging.info("[downloader] stop_event get notified")
-
-#               file_fullpath = self.downloaded_files.get()
-#               logging.info("[downloader] downloaded file : %s", file_fullpath)
-#               logging.info("[downloader] wait for the threads")
-
-
-#               # new file process routine.
-#               scheme, netloc, path, query, fragment = urlparse.urlsplit(file_fullpath)
-#               filename = os.path.basename(path)
-
-#               sha256_hash = berlib.sha256sum(file_fullpath)
-#               apk_dest = os.path.join(self.kLOCATION, self.kGALLERY_NAME, sha256_hash[0])
-#               if not os.path.exists(apk_dest):
-#                   os.makedirs(apk_dest)
-#               try:
-#                   shutil.move(file_fullpath, apk_dest)
-#               except:
-#                   logging.info("[downloader] can not move a file. maybe it is already exists")
-
-#               #logging.info("[downloader] start to upload file : %s")
-#               apk_dest_fullpath  = os.path.join(self.kLOCATION, self.kGALLERY_NAME, sha256_hash[0], filename)
-#               apk_dest_halfpath  = os.path.join(self.kGALLERY_NAME, sha256_hash[0], filename)
-
-
-#               logging.info("[downloader] set download/visit flag")
-
-#               if ret:
-#                   logging.info("[downloader] apk(No. %d) is downloaded" % row['no'])
-#                   #logging.info("[downloader] remove a file : %s", file_fullpath)
-#                   #os.remove(file_fullpath)
-#           else:           # ret_wait
-#               logging.info("[downloader] stop_event timeout %d", wait_time)
-#               if ret:
-#                   logging.info("[downloader] apk(No. %d) is not downloaded(with visit flag set)" % row['no'])
-
-
-#               # set visit flag and go to next number
-#               # Here we need to add DB field download to tell downloaded or not
-
-            #else:
-            #    logging.info("[downloader] Can't click Fast Download button. Just set a visit flag")
-
-        web_driver.close()
-
-    def test_run(self):
-        web_driver = self.init_selenium_driver("chrome")
-
-        # overall process as follows.
-        # do the similar jobs as styner did. (gather urls and put them all in db and process each records)
-
-        # start_urls???�회?�면???�집??url(app_urls)??db???��? ??app_urls 초기??
-        for url in self.start_urls:
-            self.browse_category_and_collect(web_driver, url)
-
-            self.app_urls.clear()
-
-        web_driver.quit()
-
-    """ monitor and collect to given url """
     def test_url(self, url):
         ###
         logging.info("[downloader] start a user action thread")
-        #ret = self.download_apk(web_driver, row['url'])
-        #ThreadWorker(self.download_apk, web_driver, row['url']).start()
-        #thread_simulator = ThreadWorker(self.simulate_user_action, web_driver, row['url'])
-        thread_simulator = ThreadWorker(self.simulate_user_action, url)
+
+        thread_simulator = ThreadWorker(self.simulate_user_action, url )
         thread_simulator.start()
         thread_simulator.join()
-
-
-
-
-
-    def test_db_query(self):
-        print row['no'], row['url']
 
 
     def run(self):
@@ -386,13 +351,13 @@ class Monitor():
     def simulate_user_action(self, url):
         logging.info("[simulate_user_action] opening : %s", url)
 
-        try:
-            self.download_if_attached(url)
-        except:
-             logging.error("Browser Page load timed out")
+        #try:
+        self.download_if_attached(url)
+        #except:
+        #     logging.error("Browser Page load timed out")
              #reset the page for the next test
              #web_driver.get("about:blank")
-             return False
+        #     return False
 
         logging.info("[simulate_user_action] exit thread")
 
@@ -559,7 +524,7 @@ def process_agruments():
 
     parser.add_argument('-u', '--url',          dest='get_url', default=None, action="store_true", help='monitor a given url')
     parser.add_argument('-t', '--test-url', dest='test_url', default=None, action="store_true", help='test run with a given url')
-    parser.add_argument('-l', '--location',     dest='location',action="store", help='Location to put files')
+    parser.add_argument('-d', '--directory',     dest='directory',action="store", help='directory to put files')
     #parser.add_argument('-v', '--version',   dest='version',  default=None,   action="store_true", help='show program revision')
 
 
@@ -579,6 +544,45 @@ def verify_options(options):
 
     if options.get_apk:
         print ("get apk")
+
+
+
+""" monitor and collect """
+def test_url():
+        ###
+        logging.info("[downloader] start a user action thread")
+        mon = Robot('', '')
+        test_url = "http://gall.dcinside.com/board/view/?id=game_classic&no=6329821&page=1"
+
+        mon.test_url(test_url)
+
+def test_gallery_name():
+        gname = 'http://gall.dcinside.com/board/view/?id=game_classic'
+
+        mon = Robot('', '')
+        mon.monitor_and_get(gname)
+
+def test_new_tab():
+    gname = 'http://gall.dcinside.com/board/view/?id=game_classic'
+    mon = Robot('', '')
+    #ActionChains(mon.web_driver).send_keys(Keys.CONTROL, "t").perform()
+    ActionChains(mon.web_driver).send_keys(Keys.LEFT_CONTROL, "t").perform()
+    handle = mon.web_driver.current_window_handle
+
+    mon.web_driver.execute_script("window.open('');")
+    new_tab_handle = mon.web_driver.window_handles[-1] # -1 is the latest window handle
+    #new_tab_handle = mon.web_driver.current_window_handle
+
+    mon.get_page_no(gname)
+    #ActionChains(mon.web_driver).send_keys(Keys.LEFT_CONTROL, "w").perform()
+    mon.web_driver.execute_script("self.opener = self; window.close();")
+    #mon.web_driver.switch_to.window(new_tab_handle)
+
+    mon.web_driver.get(gname)
+    mon.web_driver.switch_to.window(handle)
+
+
+
 
 
 if __name__ == "__main__":
@@ -612,20 +616,6 @@ if __name__ == "__main__":
     args = process_agruments()
     #verify_options(args)
 
-    args.test_url = kGALLERIES[0]
-    args.location = 'test_dir'
-    mon = Monitor(args.test_url, args.location)
-    args.test_url = "http://gall.dcinside.com/board/view/?id=game_classic&no=6329821&page=1"
-    mon.test_url(args.test_url)
-
-    #mon.close_selenium()
-#   if args.get_url:
-#       logging.info("selected mode : get url")
-#       collector.run_collector_url()
-
-#   if args.get_apk:
-#       logging.info("selected mode : download apk")
-#       if args.location is None:
-#           logging.info("-l must be given to run a program")
-#           sys.exit(1)
-#       collector.run_collector_downloader()
+    #test_url()
+    test_gallery_name()
+    #test_new_tab()
